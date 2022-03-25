26e0943cf125a79fb4d7e77bd958ab557568d88f
Léo Schoukroun <leopaul36@yahoo.fr>
2022-03-25 18:21:02 +0100

    hardhat: add local vagrant network

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
 
-// If you are using MetaMask, be sure to change the chainId to 1337
+const PRIVATE_KEY = "56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027";
+
 module.exports = {
   solidity: "0.7.3",
   networks: {
     hardhat: {
       chainId: 31337
-    }
+    },
+    fuji: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      chainId: 43113,
+      // accounts: [PRIVATE_KEY]
+    },
+    vagrant: {
+      url: "http://192.168.10.11:9650/ext/bc/subnav/rpc",
+      chainId: 13213,
+      accounts: [PRIVATE_KEY]
+    },
   }
 };
